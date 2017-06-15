9587415ec8c7a0cce3454d37a40aa5e9cac6b7d6
Wim Van de Meerssche <wim.vandemeerssche@intec.ugent.be>
2017-06-15 16:33:40 +0200

    bugfor for debian jessie

--- install-ansible.sh
+++ install-ansible.sh
@@ -30,8 +30,8 @@ if grep -qi 'Debian GNU/Linux 8' /etc/issue
 then
     #special procedure on debian jessie, requiring deinstalling an automaticly installed package
     apt-get update
-    apt-get install libssl-dev libffi-dev python-dev python-pip
-    apt-get --purge remove python-cffi
+    apt-get install -y libssl-dev libffi-dev python-dev python-pip
+    apt-get --purge -y remove python-cffi
     pip install --upgrade markupsafe setuptools ansible==2.0.2.0
 fi
 
